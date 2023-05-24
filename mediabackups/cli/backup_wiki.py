6f4dfd5300c3676f2d833f99e7b9609bb5f6f684
Jcrespo <jcrespo@wikimedia.org>
2023-05-24 13:57:38 +0200

    Add tmpdir removal, now that upload is stable
    Remove the temporary & unique per-process download folder at the
    end of the process to avoid leaving garbage if the upload process
    finishes correctly.
    
    Also refactor the main procedure and add some additional error
    handling.
    
    Bug: T327157
    Change-Id: I32162f8aeb81656cf52f1bd2bcd548f1a574548d

--- mediabackups/cli/backup_wiki.py
+++ mediabackups/cli/backup_wiki.py
@@ -8,6 +8,7 @@ files from a wiki or a section.
 
 import logging
 import os
+import sys
 
 import mediabackups.Encryption
 import mediabackups.SwiftMedia
@@ -104,8 +105,63 @@ def upload_file(download_path, backup_name, s3api):
     return location
 
 
+def download_and_backup(f, tmp_dir, download_path, non_public_wikis, s3api, encryption):
+    """
+    Given a file, download it from Swift to tmp_dir locally, process it (hash, encrypt, etc)
+    and upload to backup through S3 api. Return the backup status ("backed up") and its
+    final upload path (location), or rise an exception of type DownloadException,
+    EncryptionException, DuplicateException or UploadException.
+    """
+    logger = logging.getLogger('backup')
+    download_path = download_file_from_production(f, tmp_dir)
+    handle_checksums(f, download_path)
+    backup_name = calculate_backup_storage_path(f, non_public_wikis)
+    check_file_exists(backup_name, s3api)
+    download_path = handle_encryption(f, download_path, encryption, non_public_wikis)
+    location = upload_file(download_path, backup_name, s3api)
+    logger.info('Backup of "%s" completed correctly', str(f))
+    return 'backedup', location
+
+
+def remove_tmp_dir(tmp_dir):
+    """Remove the temporary local dir created to download backups (log but ignore errors)"""
+    logger = logging.getLogger('backup')
+    try:
+        os.rmdir(tmp_dir)
+    except FileNotFoundError:
+        logger.error('Temporary download directory %s could not be deleted: it was not found', tmp_dir)
+    except OSError:
+        logger.error('Temporary download directory %s could not be deleted: it is not empty', tmp_dir)
+
+
+def create_tmp_dir(storage_config):
+    """
+    Creates a unique (for the process) subdirectory inside the path given in the storage
+    configuration, otherwise use TMPDIR as default; and return it; exit if it fails.
+    We do not use a real temporary disk because we need it a) to make sure it is on disk,
+    not a ramdisk or other virtual mapping of the filesystem to memory, and b) the partition
+    is large enough to contain (potentially multiple) 4GB video files.
+    """
+    # use the process PID to guarantee uniqueness
+    logger = logging.getLogger('backup')
+    tmp_dir = os.path.join(storage_config.get('tmpdir', TMPDIR), str(os.getpid()))
+    try:
+        os.mkdir(tmp_dir)
+    except PermissionError:
+        logger.error("The download dir %s could not be created due to a permission problem", tmp_dir)
+        sys.exit(255)
+    except FileExistsError:
+        logger.error("The download dir %s could not be created because it already exists", tmp_dir)
+        sys.exit(254)
+    except FileNotFoundError:
+        logger.error("The download dir %s could not be created because its parent dir does not exist", tmp_dir)
+        sys.exit(253)
+    return tmp_dir
+
+
 def main():
     """Read list of pending files to backup, download, process them and upload to S3"""
+    # Setup all needed interfaces
     logger = logging.getLogger('backup')
     logging.basicConfig(format='[%(asctime)s] %(levelname)s:%(name)s %(message)s',
                         filename='backup_store.log', level=logging.WARNING)
@@ -113,25 +169,19 @@ def main():
     storage_config = read_yaml_config(STORAGE_CONFIG_FILE)
     s3api = mediabackups.S3.S3(storage_config)
     encryption = mediabackups.Encryption.Encryption(storage_config['identity_file'])
-
-    tmp_dir = os.path.join(storage_config.get('tmpdir', TMPDIR), str(os.getpid()))
-    os.mkdir(tmp_dir)  # Ignore failures creating the dir
+    tmp_dir = create_tmp_dir(storage_config)
     metadata.connect_db()
     non_public_wikis = metadata.get_non_public_wikis()
+
+    # loop over pending files to be backed up
     for batch in metadata.process_files():
         status_list = list()
         for file_id, f in batch.items():
             location = None
             download_path = None
             try:
-                download_path = download_file_from_production(f, tmp_dir)
-                handle_checksums(f, download_path)
-                backup_name = calculate_backup_storage_path(f, non_public_wikis)
-                check_file_exists(backup_name, s3api)
-                download_path = handle_encryption(f, download_path, encryption, non_public_wikis)
-                location = upload_file(download_path, backup_name, s3api)
-                new_status = 'backedup'
-                logger.info('Backup of "%s" completed correctly', str(f))
+                new_status, location = download_and_backup(f, tmp_dir, download_path, non_public_wikis,
+                                                           s3api, encryption)
             except DownloadException:
                 logger.error('Download of "%s" failed', str(f))
                 new_status = 'error'
@@ -155,7 +205,10 @@ def main():
                 except OSError:
                     pass  # ignoring errors as the file may not exist
         metadata.update_status(status_list)
+
+    # cleanup and finish
     metadata.close_db()
+    remove_tmp_dir(tmp_dir)
 
 
 if __name__ == "__main__":
