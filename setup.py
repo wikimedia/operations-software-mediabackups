d3f54f903346b3917b5e0b0e281202e19ed5fdc1
Jcrespo <jcrespo@wikimedia.org>
2023-12-05 12:43:57 +0100

    Prepare for 0.3.0 release
    Bug: T352655
    Change-Id: I379add9880b607209a0e6ad1152c124ea27a15f4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 setup(
     name='mediabackups',
     description='mediabackups',
-    version='0.2.0',
+    version='0.3.0',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://phabricator.wikimedia.org/diffusion/OSMB/",
